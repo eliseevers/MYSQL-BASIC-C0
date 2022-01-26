7bdf5d7405e12a6b42d273abd7071df42d19c7f2
eliseevers <2155620@talnet.nl>
2022-01-26 21:47:50 +0100

    opdracht7

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,5 +1,21 @@
 -- Opdracht 1
+127.0.0.1/mod-mysql-basic-fifa2018/players/		http://localhost/phpmyadmin/index.php?route=/table/sql&db=mod-mysql-basic-fifa2018&table=players
+
+   Weergave van records 0 -  1 (2 totaal, Query duurde 0,0022 seconden.)
+
+
+SELECT * FROM players WHERE id = 198329;
+
+
+id	name	age	nationality	club	value	wage	
+198329	Rodrigo 	26	Spain 	Valencia CF 	13000000	36000	
+198329	Rodrigo 	26	Spain 	Valencia CF 	13000000	36000	
 
 -- Opdracht 2
-
+DELETE FROM players WHERE id=198329;
 -- Opdracht 3
+DELETE FROM players WHERE id=199304;
+DELETE FROM players WHERE id=207494;
+DELETE FROM players WHERE id=226753;
+
+ALTER TABLE `players` ADD PRIMARY KEY(`id`);
